0b736b4c90de4453b81a7346b788777803659684
Alessio Gelardi <alessiogelardi0601@gmail.com>
2023-04-03 17:13:43 +0200

    add zaino into deck-edit

--- src/app/module/deck/component/deck-edit/deck-edit.component.ts
+++ src/app/module/deck/component/deck-edit/deck-edit.component.ts
@@ -6,6 +6,8 @@ import { Card, Deck } from 'src/app/module/interface/card';
 import { MessageService } from 'src/app/services/swalAlert/message.service';
 import { DeckService } from '../../services/httpservices/deck.service';
 import { StateDeckService } from '../../services/state/state-deck.service';
+import { StatePlayerService } from 'src/app/services/state/state-player.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-deck-edit',
@@ -18,14 +20,21 @@ export class DeckEditComponent implements OnInit {
 
   deck: Deck | undefined;
   deckId: string | undefined;
+  playerId: string | undefined;
 
   viewFilter = false;
   filterName:string | undefined;
 
+  zaino: Card[]=[];
+
+  sliceLimit: number | undefined;
+  sliceStart: number = 0;
+  sliceEnd: number = 60;
+  slice: number = 60;
+
   constructor(private router: Router,
-    private spinnerService: NgxSpinnerService,
     private deckStateService: StateDeckService,
-    private deckService: DeckService,
+    private playerStateService: StatePlayerService,
     private messageService: MessageService) { }
 
   ngOnInit(): void {
@@ -45,6 +54,24 @@ export class DeckEditComponent implements OnInit {
     this.deckId = '634ebd932a1bb2f2d4f921ef'; //this.route.snapshot.paramMap.get('id')!;
     this.deckStateService.getDeck(this.deckId).then((resp) => {
       this.deck = resp;
+    });
+
+    this.playerId = "63459b3a4b4c877f5a46f43e"; //this.route.snapshot.paramMap.get('playerId')
+
+    this.playerStateService.getZaino(this.playerId!).then((resp) => {
+      if(resp) {
+        this.zaino = resp;
+        this.sliceLimit = this.zaino.length;
+      } else {
+        //TO-DO gestione degli errori
+        /*
+        if(resp.status===402) {
+          this.swalAlert('Attenzione!','non ho trovato nulla con questo id, probabilmente devi fare la registrazione','error');
+        }
+        */
+
+        this.messageService.alert('Attenzione!','Errore durante la chiamata getZaino','error');
+      }
     });
   }
 
@@ -102,6 +129,39 @@ export class DeckEditComponent implements OnInit {
   
   showCard(card:Card) {
     this.messageService.showDetailCard(card);
+  }
+
+  addCard(card:Card) {
+    if(card && card.type == 8388641 || card.type === 97 || card.type === 8225) { //TO-DO
+      this.deck?.extra.push(card);
+    } else if (card) {
+      Swal.fire({
+        title: 'Dove vuoi aggiungere questa carta?',
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: 'Main',
+        denyButtonText: 'Side',
+        cancelButtonText: 'Annulla',
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this.deck?.main.push(card)
+        } else if (result.isDenied) {
+          this.deck?.side.push(card)
+        }
+      })
+      
+    }
+
+  }
+
+  backSlice() {
+    this.sliceEnd -= this.slice;
+    this.sliceStart -= this.slice;
+  }
+
+  continueSlice() {
+    this.sliceStart += this.slice;
+    this.sliceEnd += this.slice;
   }
 
 }
