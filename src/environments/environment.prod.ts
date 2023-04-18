0905032a877e5f38c07f91fab2f7f3d3da6295a9
Alessio Gelardi <alessiogelardi0601@gmail.com>
2023-04-18 19:06:59 +0200

    build for fun

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,6 @@ export const environment = {
   baseUrlLogin: 'http://localhost:5001/login/',
   baseUrlPlayer: 'http://localhost:5001/player/',
   baseUrlDeck: 'http://localhost:5001/deck/',
+  baseUrlMarket: 'http://localhost:5001/market/',
+  baseUrlNotifier: 'http://localhost:5001/notifier/',
 };
