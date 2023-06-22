d94bed47411901ae2012bcf87c569315adfa682c
Alessio Gelardi <alessiogelardi0601@gmail.com>
2023-06-22 16:26:27 +0200

    NI13 - check in accept request for PUNTATA

--- src/app/module/notifier/enum/status.ts
+++ src/app/module/notifier/enum/status.ts
@@ -2,5 +2,5 @@ export enum Status {
     INVIATO = 1, //richiesta inviata
     ACCETTATO = 2, //in attesa di vincitore
     COMPLETATO = 3, //vincitore decretato
-    ANNULLATO = 4 // richiesta annullata
+    RIFIUTATO = 4 // richiesta rifiutata
 }
