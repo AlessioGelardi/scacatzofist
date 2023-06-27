46104512d5f145fc86a476309b779add54ab44ba
Alessio Gelardi <alessiogelardi0601@gmail.com>
2023-06-27 18:51:17 +0200

    NI5 - possibilità di acquistare starter deck

--- src/app/module/market/services/state/state-market.service.ts
+++ src/app/module/market/services/state/state-market.service.ts
@@ -168,7 +168,7 @@ export class StateMarketService {
 
 
     return response;
-  }
+  }  
 
   async buyCredits(playerId:string, numCredits:number) {
     this.spinnerService.show();
@@ -176,23 +176,6 @@ export class StateMarketService {
 
     try {
       response = await firstValueFrom(this.marketService.acquistaCrediti(playerId,numCredits));
-      this.spinnerService.hide();
-    } catch (error: any) {
-      /* TO-DO [WinError 3] Impossibile trovare il percorso specificato: 'deck\\\\Ingranaggio Antico1.ydk' -> 'deck\\\\Ingranaggio Antico.ydk'*/
-      response = error;
-      this.spinnerService.hide();
-    }
-
-
-    return response;
-  }
-
-  async buyDailyPack(request: any) {
-    this.spinnerService.show();
-    let response;
-
-    try {
-      response = await firstValueFrom(this.marketService.acquistaDaily(request));
       this.spinnerService.hide();
     } catch (error: any) {
       /* TO-DO [WinError 3] Impossibile trovare il percorso specificato: 'deck\\\\Ingranaggio Antico1.ydk' -> 'deck\\\\Ingranaggio Antico.ydk'*/
