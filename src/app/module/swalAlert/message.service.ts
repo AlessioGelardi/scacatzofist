94eb790eb4cddce25781ae52eb79e06e91707462
Alessio_g <a.gelardi@alfasoft.it>
2023-09-12 17:39:41 +0200

    BUG34 - Togliere undefined nel dettaglio carte quando visualizzo il dettaglio di una carta priva di Attributo,Razza ATK e DEF

--- src/app/module/swalAlert/message.service.ts
+++ src/app/module/swalAlert/message.service.ts
@@ -18,16 +18,33 @@ export class MessageService {
   }
 
   public showDetailCard(card:Card) {
-    Swal.fire({
-      heightAuto: false,
-      backdrop: false,
-      title: card.name,
-      color: '#3e3d3c',
-      background: '#cdcccc',
-      html: '<p>Livello: <strong>'+card.level+'</strong> Attributo: <strong>'+Attributi[card.attribute]+'</strong> Razza: <strong>'+Razze[card.race]+'</strong></p><p>'+card.description+'</p><p>ATK: <strong>'+card.atk+'</strong> DEF: <strong>'+card.def+'</strong></p>',
-      imageUrl: 'https://images.ygoprodeck.com/images/cards/'+card.id+'.jpg',
-      imageWidth: 160
-    })
+    if(card.level>0) {
+      Swal.fire({
+        heightAuto: false,
+        backdrop: false,
+        title: card.name,
+        color: '#3e3d3c',
+        background: '#cdcccc',
+        html: '<p>Livello: <strong>'+card.level+
+        '</strong> Attributo: <strong>'+Attributi[card.attribute]+
+        '</strong> Razza: <strong>'+Razze[card.race]+'</strong></p><p>'+
+        card.description+'</p><p>ATK: <strong>'+card.atk+'</strong> DEF: <strong>'+card.def+'</strong></p>',
+        imageUrl: 'https://images.ygoprodeck.com/images/cards/'+card.id+'.jpg',
+        imageWidth: 160
+      })
+    } else {
+      Swal.fire({
+        heightAuto: false,
+        backdrop: false,
+        title: card.name,
+        color: '#3e3d3c',
+        background: '#cdcccc',
+        html: '<p>'+ card.description+'</p>',
+        imageUrl: 'https://images.ygoprodeck.com/images/cards/'+card.id+'.jpg',
+        imageWidth: 160
+      })
+    }
+
   }
 
   public showDetailPack(pack:SellPack) {
