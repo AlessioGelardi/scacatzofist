b43219fb1c781613ad2d987217db3042d8eecc78
Alessio_g <a.gelardi@alfasoft.it>
2023-10-07 12:58:11 +0200

    M54 - Web App, per grafica telefono

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -43,10 +43,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     DragDropModule,
     TradeModule,
     ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: !isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      enabled: false,
     })
   ],
   providers: [],
